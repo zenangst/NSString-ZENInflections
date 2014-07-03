4c9f9da0f54534e93f340e3f4acc83a136f779b5
Christoffer Winterkvist <chris@zenangst.com>
2014-07-03 19:49:50 +0200

    Add NSString-ZENInflectionsTests

---
+++ NSString-ZENInflectionsTests/NSString_ZENInflectionsTests.m
@@ -0,0 +1,34 @@
+//
+//  NSString_ZENInflectionsTests.m
+//  NSString-ZENInflectionsTests
+//
+//  Created by Christoffer Winterkvist on 7/3/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSString_ZENInflectionsTests : XCTestCase
+
+@end
+
+@implementation NSString_ZENInflectionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
