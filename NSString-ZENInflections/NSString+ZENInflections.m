43cd3a26c6a1e75f31a3b8f4a4a85e4acef6c175
Elvis Nuñez <elvisnunez@me.com>
2014-12-09 11:20:51 +0100

    Clean up

--- NSString-ZENInflections/NSString+ZENInflections.m
+++ NSString-ZENInflections/NSString+ZENInflections.m
@@ -1,14 +1,7 @@
-//
-//  NSString+ZENInflections.m
-//  Mine Ansatte
-//
-//  Created by Christoffer Winterkvist on 7/2/14.
-//  Copyright (c) 2014 Hyper. All rights reserved.
-//
-
 #import "NSString+ZENInflections.h"
 
 @implementation NSString (ZENInflections)
+
 #pragma mark - Class methods
 
 + (NSString *)zen_stringWithCamelCase:(NSString *)string
--- NSString-ZENInflectionsTests/NSString_ZENInflectionsTests.m
+++ NSString-ZENInflectionsTests/NSString_ZENInflectionsTests.m
@@ -1,12 +1,5 @@
-//
-//  NSString_ZENInflectionsTests.m
-//  NSString-ZENInflectionsTests
-//
-//  Created by Christoffer Winterkvist on 7/3/14.
-//
-//
+@import XCTest;
 
-#import <XCTest/XCTest.h>
 #import "NSString+ZENInflections.h"
 
 @interface NSString_ZENInflectionsTests : XCTestCase
@@ -14,18 +7,6 @@
 @end
 
 @implementation NSString_ZENInflectionsTests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
 
 - (void)testCamelCase
 {
